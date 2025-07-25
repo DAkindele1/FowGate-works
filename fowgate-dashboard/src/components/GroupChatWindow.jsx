85c26708bad036e4bce3c8dfb9ce806d2f5530e5
DAkindele1 <148255025+DAkindele1@users.noreply.github.com>
2025-07-25 14:32:15 +0000

    Update14

--- fowgate-dashboard/src/components/GroupChatWindow.jsx
+++ fowgate-dashboard/src/components/GroupChatWindow.jsx
@@ -238,15 +238,15 @@ export default function GroupChatWindow({ chat, onClose, onSendMessage, togglePi
       {/* Header */}
       <div className="bg-[#EB4335] text-white flex items-center gap-2 px-6 py-4">
         <div className="flex items-center gap-2">
-        <button
-          onClick={() => setChatToDelete(null)}
-          className="text-white text-lg font-bold hover:text-gray-200"
-        >
-          X
-        </button>
         <img src={Delete} alt="Delete Icon" className="w-5 h-5" />
         <h2 className="text-base font-semibold">Delete</h2>
       </div>
+        <button
+          onClick={() => setChatToDelete(null)}
+          className="text-white text-lg font-light hover:text-gray-200"
+        >
+          X
+        </button>
       </div>
 
       {/* Body */}
